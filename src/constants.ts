f3cf8f2682eda764b420e3007e5685d3a0c31d5c
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2024-04-13 21:42:19 +0200

    done with all small steps for ingredients migration
    Now you can read a file of 1000 ingredients that contains names and IDs from spoonacular api
    
    get image of the ingredient from spoonacular and upload it to cloudinary
    
    get ingredient details using name from ninja api
    
    get ingredient category and image name from spoonacular api

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const CLOUDINARY = 'Cloudinary';
-export const CLOUDINARY_EXERCISES_FOLDER_NAME='exercises';
-export const CLOUDINARY_INGREDIENTS_FOLDER_NAME='ingredients';
+export const CLOUDINARY_EXERCISES_FOLDER_NAME = 'exercises';
+export const CLOUDINARY_INGREDIENTS_FOLDER_NAME = 'Ingredients';
--- src/ingrediant/ingredient.controller.ts
+++ src/ingrediant/ingredient.controller.ts
@@ -1,33 +1,42 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Res, Param, BadRequestException } from '@nestjs/common';
 import { IngredientInfoDto } from 'src/dtos/ingredient.dto';
 import { IngredientService } from './ingredient.service';
 import * as request from 'request';
 import { headers } from 'next/headers';
-import { Param } from '@nestjs/common';
 import axios from 'axios';
 import { Response } from 'express';
+import { CLOUDINARY_INGREDIENTS_FOLDER_NAME } from 'src/constants';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { v2 } from 'cloudinary';
+import { Readable } from 'typeorm/platform/PlatformTools';
 
 @Controller()
 export class IngredientController {
-    ingredientService;
-    ninja_api_url = 'https://api.api-ninjas.com/v1/nutrition?query=';
-    ninja_api_key = '6R4a7wGbVEky7vQR8XG8Pg==P1zy9b4aNrXh1ed7';
-    sponacular_api_url = 'https://api.spoonacular.com/food/ingredients/';
-    sponacular_api_key = 'bab678bfea8346c5b1723b6c14226d7d';
+  ingredientService;
+  cloudinary;
+  ninja_api_url;
+  ninja_api_key;
+  spoonacular_api_url;
+  spoonacular_api_key;
 
-    constructor() {
-        this.ingredientService = new IngredientService();
-    }
+  constructor() {
+    this.ingredientService = new IngredientService();
+    this.cloudinary = new CloudinaryService();
+    this.ninja_api_url = process.env.NINJA_API_URL;
+    this.ninja_api_key = process.env.NINJA_API_KEY;
+    this.spoonacular_api_url = process.env.SPOONACULAR_API_URL;
+    this.spoonacular_api_key = process.env.SPOONACULAR_API_URL;
+  }
 
-    // Call the function with the path to your CSV file
-    @Get('read-ingredients')
-    async readIngredients(): Promise<IngredientInfoDto[]> {
-        const filePath = 'src/top-1k-ingredients.csv';
-        const ingredients = await this.ingredientService.processIngredientsFile(filePath);
-        console.log(ingredients);
-        console.log('Number of ingredients:', ingredients.length);
-        return ingredients;
-    }
+  // Call the function with the path to your CSV file
+  @Get('read-ingredients')
+  async readIngredients(): Promise<IngredientInfoDto[]> {
+    const filePath = 'src/top-1k-ingredients.csv';
+    const ingredients = await this.ingredientService.processIngredientsFile(filePath);
+    console.log(ingredients);
+    console.log('Number of ingredients:', ingredients.length);
+    return ingredients;
+}
 
     @Get('get_ingredient_info/:name')
     async getIngredientHealtyDetails(@Param('name') name: string): Promise<any> {
@@ -41,6 +50,7 @@ export class IngredientController {
 
             console.log(response.data);
             return response.data;
+
         } catch (error) {
             if (error.response) {
                 console.error(`Error: ${error.response.status} ${error.response.data}`);
@@ -60,9 +70,9 @@ export class IngredientController {
     async getIngredientDetails(@Param('id') id: string): Promise<any> {
         const options = {
             method: 'GET',
-            url: `${this.sponacular_api_url}${id}/information`,
+            url: `${this.spoonacular_api_url}${id}/information`,
             headers: {
-                'x-api-key': this.sponacular_api_key,
+                'x-api-key': this.spoonacular_api_key,
             }
         };
 
@@ -79,19 +89,48 @@ export class IngredientController {
     @Get('get_image/:imageName')
     async getImage(@Param('imageName') imageName: string, @Res() res: Response): Promise<void> {
         try {
-
             const imageResponse = await axios.get(`https://img.spoonacular.com/ingredients_500x500/${imageName}`, {
+                responseType: 'stream', // Set response type to stream to directly pipe it to response
                 headers: {
-                    'x-api-key': this.sponacular_api_key,
-                }
+                    'x-api-key': this.spoonacular_api_key,
+                },
             });
-            res.set('Content-Type', imageResponse.headers['content-type']);
-            res.send(imageResponse.data);
+
+            res.setHeader('Content-Type', 'image/jpeg');                // Set content type header for the image
+            imageResponse.data.pipe(res);        // Pipe the image data directly to the response
+            
+            const link = await this.uploadIngredientImageToCloudinary(imageResponse.data);
+            console.log(link);
+
+            // return Buffer.from(imageResponse.data, 'binary');
         } catch (error) {
             console.error('Error fetching image:', error);
-            res.status(500).send('Error fetching image');
+            // Return an error response if something goes wrong
+            res.status(500).send('Internal server error');
         }
     }
+
+
+    
+    async uploadIngredientImageToCloudinary(imageStream: Readable): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            const uploadStream = v2.uploader.upload_stream(
+                {
+                    folder: CLOUDINARY_INGREDIENTS_FOLDER_NAME,
+                },
+                (error, result) => {
+                    if (error) {
+                        reject(new Error('Image upload failed'));
+                    } else {
+                        resolve(result.secure_url);
+                    }
+                }
+            );
+    
+            imageStream.pipe(uploadStream);
+        });
+    }
+
 
 
 }
