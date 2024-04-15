56a1d82c05d1952d5d924de70154a603d78eae96
AntoineTalaat <es-antoinetalaat2024@alexu.edu.eg>
2024-04-15 20:25:47 +0200

    Update population.module.ts

--- src/population/population.module.ts
+++ src/population/population.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
 import { PopulationController } from './controller/population.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import typeorm from 'src/config/typeorm';
+import { IngredientController } from 'src/nutrition_side/ingredient/ingredient.controller';
+import { IngredientService } from 'src/nutrition_side/ingredient/ingredient.service';
+import { RoutesController } from 'src/routes/routes.controller';
+import { UsersModule } from 'src/users/users.module';
+import { CloudinaryModule } from 'src/utils/cloudinary/cloudinary.module';
+import { ImageService } from 'src/utils/image/image.service';
+import { ExerciseModule } from 'src/workout_side/exercise/exercise.module';
+import { ExerciseService } from 'src/workout_side/exercise/exercise.service';
+import { WorkoutController } from 'src/workout_side/workout/workout.controller';
+import { AppService } from 'src/app.service';
 
 @Module({
-  controllers: [PopulationController]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+
+    CloudinaryModule,
+    ExerciseModule,
+    UsersModule,
+  ],
+  controllers: [PopulationController, WorkoutController, IngredientController, RoutesController],
+  providers: [AppService, ImageService, ExerciseService, IngredientService],
 })
 export class PopulationModule {}
