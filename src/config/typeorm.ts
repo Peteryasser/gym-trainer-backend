28d6ffc0555007c1bc05b502037f7c0db98b332d
Peteryasser <96436582+Peteryasser@users.noreply.github.com>
2024-04-15 18:26:09 +0200

    Supabase Connection Done!!

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -3,14 +3,13 @@ import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 dotenvConfig({ path: '.env' });
-
 const config = {
   type: 'postgres',
   host: `${process.env.DATABASE_HOST}`,
   port: `${process.env.DATABASE_PORT}`,
   username: `${process.env.DATABASE_USERNAME}`,
   password: `${process.env.DATABASE_PASSWORD}`,
-  database: `${process.env.DATABASE_NAME}`,
+  database: `${process.env.DATABASE}`,
   entities: ['dist/entity/**/*{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
