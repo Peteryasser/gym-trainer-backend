d67c7c86a0f607af74db9fb0347797620ae0952b
yarahossamboo <yarahossam222@gmail.com>
2024-04-22 02:34:07 +0200

    add global pipes config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 
