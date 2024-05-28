bbbd62ea180222cec40271acc0973d1e4bef3486
yarahossamboo <yarahossam222@gmail.com>
2024-05-28 23:05:06 +0300

    add subscriptions module

--- src/user_subscriptions/user_subscriptions.module.ts
+++ src/user_subscriptions/user_subscriptions.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { UserSubscriptionsService } from './service/user-subscriptions.service';
+import { UserSubscriptionController } from './controller/user-subscriptions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSubscription } from '../entity/user-subscription.entity';
+import { PackagesService } from '../packages/services/packages.service';
+import { Package } from '../entity/coach-package.entity';
+import { CoachesModule } from '../users/coaches/coaches.module';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Package, UserSubscription]),
+    CoachesModule,
+  ],
+
+  controllers: [UserSubscriptionController],
+  providers: [UserSubscriptionsService, PackagesService],
+})
 export class UserSubscriptionsModule {}
