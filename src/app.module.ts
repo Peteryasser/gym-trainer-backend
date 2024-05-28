131ba2514f3cdb92f47a75921af3cbc23efe8759
yarahossamboo <yarahossam222@gmail.com>
2024-05-28 23:01:09 +0300

    import coaches module

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,7 @@ import { UsersModule } from './users/users.module';
 import { PopulationModule } from './population/population.module';
 import { PackagesModule } from './packages/packages.module';
 import { UserSubscriptionsModule } from './user_subscriptions/user_subscriptions.module';
+import { CoachesModule } from './users/coaches/coaches.module';
 
 @Module({
   imports: [
@@ -36,6 +37,7 @@ import { UserSubscriptionsModule } from './user_subscriptions/user_subscriptions
     ExerciseModule,
     AuthModule,
     UsersModule,
+    CoachesModule,
     PopulationModule,
     PackagesModule,
     UserSubscriptionsModule,
