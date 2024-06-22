bb75d05fd9304f3841447abf6f1e6a64022cfc2d
yarahossamboo <yarahossam222@gmail.com>
2024-06-22 23:42:34 +0300

    update crypto module

--- src/crypto/crypto.module.ts
+++ src/crypto/crypto.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CryptoService } from './service/crypto.service';
+import { CryptoController } from './controller/crypto.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserKeys } from 'src/entity/user-keys.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserKeys])],
   providers: [CryptoService],
+  controllers: [CryptoController],
 })
 export class CryptoModule {}
