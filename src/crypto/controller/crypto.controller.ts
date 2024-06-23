367689b49e97c2c0aac16104ff2ffa0cc27e819a
yarahossamboo <yarahossam222@gmail.com>
2024-06-23 16:49:17 +0300

    use guard in crypto controller

--- src/crypto/controller/crypto.controller.ts
+++ src/crypto/controller/crypto.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CryptoService } from '../service/crypto.service';
 import { GetUser } from 'src/auth/decorators/get-user.decorator';
 import { User } from 'src/entity/user.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('crypto')
 export class CryptoController {
   constructor(private readonly crptoService: CryptoService) {}
