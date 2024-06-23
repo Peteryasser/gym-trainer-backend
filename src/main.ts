135b3b48412fb25c3fd1a48b301bf3b9b5381d11
yarahossamboo <yarahossam222@gmail.com>
2024-06-23 18:59:41 +0300

    pretty main

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,12 @@ import * as bodyParser from 'body-parser';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import * as fs from 'fs';
 
-
 async function bootstrap() {
   const httpsOptions = {
     key: fs.readFileSync('./key.pem'),
     cert: fs.readFileSync('./cert.pem'),
   };
-  const app = await NestFactory.create(AppModule,{
+  const app = await NestFactory.create(AppModule, {
     httpsOptions,
   });
   app.setGlobalPrefix('api');
