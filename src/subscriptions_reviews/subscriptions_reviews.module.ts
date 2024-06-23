559fa7eb13a3f861f53e80d64e30427ba91a0bdf
yarahossamboo <yarahossam222@gmail.com>
2024-06-23 17:47:36 +0300

    update reviews module

--- src/subscriptions_reviews/subscriptions_reviews.module.ts
+++ src/subscriptions_reviews/subscriptions_reviews.module.ts
@@ -3,8 +3,9 @@ import { SubscriptionReviewController } from './controller/subscriptions_reviews
 import { SubscriptionsReviewsService } from './service/subscriptions_reviews.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SubscriptionReview } from 'src/entity/subscription-review.entity';
+import { Coach } from 'src/entity/coach.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([SubscriptionReview])],
+  imports: [TypeOrmModule.forFeature([SubscriptionReview, Coach])],
   controllers: [SubscriptionReviewController],
   providers: [SubscriptionsReviewsService],
 })
