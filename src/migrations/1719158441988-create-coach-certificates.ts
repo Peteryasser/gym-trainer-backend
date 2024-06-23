050b0e39d0779ba0626ab2ef2e972a0b29913af0
yarahossamboo <yarahossam222@gmail.com>
2024-06-23 21:24:14 +0300

    add coach certificates migration

---
+++ src/migrations/1719158441988-create-coach-certificates.ts
@@ -0,0 +1,76 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateCoachCertificates1719158441988
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: 'coach_certificates',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+
+          { name: 'coachId', type: 'int', isNullable: false },
+          {
+            name: 'title',
+            type: 'varchar',
+            length: '100',
+            isNullable: false,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'certificate_url',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'coach_certificates',
+      new TableForeignKey({
+        columnNames: ['coachId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'coaches',
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('coach_certificates');
+
+    const foreignKeyCoach = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('coachId') !== -1,
+    );
+
+    await queryRunner.dropForeignKey('coach_certificates', foreignKeyCoach);
+    await queryRunner.dropTable('coach_certificates');
+  }
+}
