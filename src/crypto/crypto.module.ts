7c3194ab79b0ed646436cc1383642d7902f6c3b8
yarahossamboo <yarahossam222@gmail.com>
2024-06-23 04:03:31 +0300

    rmv unused crypto controller

--- src/crypto/crypto.module.ts
+++ src/crypto/crypto.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CryptoService } from './service/crypto.service';
-import { CryptoController } from './controller/crypto.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserKeys } from 'src/entity/user-keys.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserKeys])],
   providers: [CryptoService],
-  controllers: [CryptoController],
+  controllers: [],
 })
 export class CryptoModule {}
