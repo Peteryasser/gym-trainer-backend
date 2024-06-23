7581811312ac3ed39cdbf5ba8f6b991c531d0612
yarahossamboo <yarahossam222@gmail.com>
2024-06-23 16:27:46 +0300

    update reviews controller return datatypes

--- src/subscriptions_reviews/controller/subscriptions_reviews.controller.ts
+++ src/subscriptions_reviews/controller/subscriptions_reviews.controller.ts
@@ -8,17 +8,20 @@ import {
   Body,
   UseGuards,
   ParseIntPipe,
+  Query,
 } from '@nestjs/common';
 import { SubscriptionsReviewsService } from '../service/subscriptions_reviews.service';
-import { CreateReviewDto } from './dtos/create-review.dto';
-import { UpdateReviewDto } from './dtos/update-review.dto';
-import { SubscriptionReview } from './subscription-review.entity';
+import { CreateReviewDto } from '../dtos/create-review.dto';
+import { UpdateReviewDto } from '../dtos/update-review.dto';
 import { User } from '../../entity/user.entity';
 import { Coach } from '../../entity/coach.entity';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.auth.guard';
 import { GetUser } from '../../auth/decorators/get-user.decorator';
+import { ReviewFilterDto } from '../dtos/review-filter.dto';
+import { ReviewDto } from '../dtos/review.dto';
+import { PaginatedResultDto } from 'src/dtos/paginatied-result.dto';
 
-@Controller('subscription_reviews')
+@Controller('user_reviews')
 @UseGuards(JwtAuthGuard)
 export class SubscriptionReviewController {
   constructor(private readonly reviewService: SubscriptionsReviewsService) {}
@@ -27,38 +30,37 @@ export class SubscriptionReviewController {
   async create(
     @Body() createReviewDto: CreateReviewDto,
     @GetUser() user: User,
-  ): Promise<SubscriptionReview> {
-    return this.reviewService.create(createReviewDto, user);
+  ): Promise<ReviewDto> {
+    return this.reviewService.create(user, createReviewDto);
   }
 
   @Get()
-  async getAll(): Promise<SubscriptionReview[]> {
-    return this.reviewService.getAll();
+  async getAll(
+    @Query() filterDto: ReviewFilterDto,
+    @GetUser() user: Coach,
+  ): Promise<PaginatedResultDto<ReviewDto>> {
+    return this.reviewService.getAll(user.id, filterDto);
   }
 
   @Get('coach/:id')
   async getByCoachId(
+    @Query() filterDto: ReviewFilterDto,
     @Param('id', ParseIntPipe) id: number,
-  ): Promise<SubscriptionReview[]> {
-    return this.reviewService.getByCoachId(id);
+  ): Promise<PaginatedResultDto<ReviewDto>> {
+    return this.reviewService.getAll(id, filterDto);
   }
 
-  @Get('package/:id')
-  async getByPackageId(
-    @Param('id', ParseIntPipe) id: number,
-  ): Promise<SubscriptionReview[]> {
-    return this.reviewService.getByPackageId(id);
-  }
-
-  @Get('user')
-  async getByUser(@GetUser() user: User): Promise<SubscriptionReview[]> {
-    return this.reviewService.getByUser(user);
+  @Get('user/my_reviews')
+  async getByUser(
+    @GetUser() user: User,
+  ): Promise<PaginatedResultDto<ReviewDto>> {
+    const filterDto = new ReviewFilterDto();
+    filterDto.userId = user.id;
+    return this.reviewService.getAll(undefined, filterDto);
   }
 
   @Get(':id')
-  async getById(
-    @Param('id', ParseIntPipe) id: number,
-  ): Promise<SubscriptionReview> {
+  async getById(@Param('id', ParseIntPipe) id: number): Promise<ReviewDto> {
     return await this.reviewService.getById(id);
   }
 
@@ -67,8 +69,8 @@ export class SubscriptionReviewController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateReviewDto: UpdateReviewDto,
     @GetUser() user: User,
-  ): Promise<SubscriptionReview> {
-    return await this.reviewService.update(id, updateReviewDto, user);
+  ): Promise<ReviewDto> {
+    return await this.reviewService.update(user, id, updateReviewDto);
   }
 
   @Delete(':id')
@@ -76,7 +78,7 @@ export class SubscriptionReviewController {
     @Param('id', ParseIntPipe) id: number,
     @GetUser() user: User,
   ): Promise<any> {
-    await this.reviewService.delete(id, user);
+    await this.reviewService.delete(user, id);
 
     return { success: true, message: 'Review deleted successfully' };
   }
