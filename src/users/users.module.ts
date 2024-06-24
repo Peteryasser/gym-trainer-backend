57e9016dcdbc732fcbe04d94636ae4d9318d65f3
yarahossamboo <yarahossam222@gmail.com>
2024-06-24 22:46:26 +0300

    add get my coaches endpoint

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,11 @@ import { UsersService } from './service/users.service';
 import { DevicesService } from './service/devices.service';
 import { User } from '../entity/user.entity';
 import { Device } from '../entity/device.entity';
+import { UsersController } from './controller/users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Device])],
+  controllers: [UsersController],
   providers: [UsersService, DevicesService],
   exports: [TypeOrmModule, UsersService],
 })
