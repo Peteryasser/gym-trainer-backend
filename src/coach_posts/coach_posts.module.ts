db2681f53f5483882f3915c05a14e3ec5e56ef3e
yarahossamboo <yarahossam222@gmail.com>
2024-06-24 06:59:11 +0300

    update coach module imports

--- src/coach_posts/coach_posts.module.ts
+++ src/coach_posts/coach_posts.module.ts
@@ -3,7 +3,7 @@ import { PostsModule } from './posts/posts.module';
 import { PostLikesModule } from './likes/posts_likes.module';
 import { PostMultimediaModule } from './multimedia/posts_multimedia.module';
 
-@Module({})
-export class CoachPostsModule {
-  imports: [PostsModule, PostLikesModule, PostMultimediaModule];
-}
+@Module({
+  imports: [PostsModule, PostLikesModule, PostMultimediaModule],
+})
+export class CoachPostsModule {}
