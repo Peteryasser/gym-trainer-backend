8b3d22a01bb48280e3af29f431966a3e0f3588a4
yarahossamboo <yarahossam222@gmail.com>
2024-06-26 12:02:43 +0300

    add keys dto and update auth response dto

--- src/auth/dtos/user.auth.response.dto.ts
+++ src/auth/dtos/user.auth.response.dto.ts
@@ -1,11 +1,15 @@
 import { UserDto } from '../../users/dtos/user.dto';
+import { UserKeysDto } from './user-keys.dto';
 
 export class UserAuthResponseDto {
   user: UserDto;
   token: string;
+  keys: UserKeysDto;
 
-  constructor(token: string, user: UserDto) {
+  constructor(token: string, user: UserDto, keys: any = null) {
     this.user = user;
     this.token = token;
+
+    if (keys) this.keys = new UserKeysDto(keys.publicKey, keys.privateKey);
   }
 }
